f9b1919e0b13418369c8edbf4f262c3ff9135ee9
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-09-18 09:38:23 +0800

    🐞 fix: useHead on init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import routes from "~pages";
-import { createHead } from "@vueuse/head";
+import { createHead, useHead } from "@vueuse/head";
 import "@akrc/fish/fish.css";
 import { createPinia } from "pinia";
+import { useStore } from "./config";
 
 const head = createHead();
 const pinia = createPinia();
@@ -12,4 +13,7 @@ export const createApp = ViteSSG(App, { routes }, async (ctx) => {
   const { app, router, routes, isClient, initialState } = ctx;
   app.use(head);
   app.use(pinia);
+  useHead({
+    title: useStore().name,
+  });
 });
